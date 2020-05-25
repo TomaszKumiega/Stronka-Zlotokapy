e115c5308cdf17a5fe6ec4b103ee3ed7d46fee22
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-05-25 15:18:54 +0200

    Init commit

---
+++ Skrypt4.js
@@ -0,0 +1,26 @@
+function sprawdzZgodnosc() {
+    tekst1 = $("#poleTekstowe1").val();
+    tekst2 = $("#poleTekstowe2").val();
+
+    if (tekst1 == tekst2) {
+        $("#poleTekstowe3").val(' ');
+        $("#poleTekstowe3").val("Tekst jest zgodny");
+    } else {
+        $("#poleTekstowe3").val(' ');
+        $("#poleTekstowe3").val("Tekst nie jest zgodny");
+    }
+}
+
+function licz() {
+    val1 = +$("#poleTekstowe4").val();
+    val2 = +$("#poleTekstowe5").val();
+
+    if (!isNaN(val1) && !isNaN(val2)) {
+        $("#poleTekstowe6").val(' ');
+        $("#poleTekstowe6").val((val1 + val2))
+    }
+}
+
+function DodajParagraf() {
+
+}
